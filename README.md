6e7154dff9a256cc60e81d2b639757f626b339ed
Timandes <timands@gmail.com>
2014-09-28 12:56:33 +0800

    import first version.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-flume-ng-file
+Flume-NG File
 =============
 
 File processing plugin for Apache Flume NG.
+
+Configuration of File Sink
+--------------------------
+
+    agent1.sinks.filesink1.channel = channel1
+    agent1.sinks.filesink1.type = timandes.flume.sinks.FileSink
+    agent1.sinks.filesink1.pathTemplate = '/var/log/'YYYYMMdd'.log'
+
---
+++ src/timandes/flume/sinks/FileSink.java
@@ -0,0 +1,86 @@
+package timandes.flume.sinks;
+
+import org.apache.flume.*;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.sink.AbstractSink;
+import java.io.FileWriter;
+import java.io.FileOutputStream;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileSink extends AbstractSink implements Configurable
+{
+    private String _pathTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(FileSink.class);
+
+    @Override
+    public void configure(Context context)
+    {
+        this._pathTemplate = context.getString("pathTemplate", "'/var/log/'YYYYMMdd'.log'");
+    }
+
+    @Override
+    public void start()
+    {
+        logger.info("FileSink.start()");
+    }
+
+    @Override
+    public void stop()
+    {
+        logger.info("FileSink.stop()");
+    }
+
+    @Override
+    public Status process() throws EventDeliveryException
+    {
+        Status retval = null;
+
+        Channel ch = getChannel();
+        Transaction txn = ch.getTransaction();
+        txn.begin();
+        try {
+            Event event = ch.take();
+            if (event == null) {
+                logger.info("No new event was found");
+                retval = Status.BACKOFF;
+            } else {
+                logger.info("New events arrived");
+                Date now = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat(this._pathTemplate);
+                String path = dateFormat.format(now);
+
+                logger.info("path=" + path);
+/*
+                FileWriter writer = new FileWriter(path, true);
+                writer.write(event.getBody());
+                writer.close();
+*/
+                FileOutputStream os = new FileOutputStream(path, true);
+                os.write(event.getBody());
+                os.write(0x0a);
+                os.close();
+
+                retval = Status.READY;
+            }
+
+            txn.commit();
+        } catch (Throwable t) {
+            logger.error(t.getMessage());
+
+            txn.rollback();
+            retval = Status.BACKOFF;
+
+            if (t instanceof Error) {
+                throw (Error)t;
+            }
+        } finally {
+            txn.close();
+        }
+
+        return retval;
+    }
+
+}
